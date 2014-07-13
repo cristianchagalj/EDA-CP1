7a002b4399ea53254a696153a359e98f838b5b74
cristianchagalj <cristianchagalj@users.noreply.github.com>
2014-07-13 20:10:46 -0300

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,10 @@
+col.names<-t(read.table(file="\\household_power_consumption.txt",nrows=1,sep=";"))
+df.power<-read.table(file="\\household_power_consumption.txt", skip=66637,nrows=2880,sep=";",col.names=col.names)
+
+dateTime <- strptime(paste(df.power$Date,df.power$Time), format="%d/%m/%Y %H:%M:%S")
+Sys.setlocale("LC_TIME", "English")
+
+#plot1
+hist(df.power$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.copy(png,file="plot1.png", width=480,height=480)
+dev.off()
