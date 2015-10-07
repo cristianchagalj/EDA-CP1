9227385c03d4066ccb11c536c173d3b54d855eae
cristianchagalj <cristianchagalj@users.noreply.github.com>
2015-10-07 18:36:52 -0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
-col.names<-t(read.table(file="\\household_power_consumption.txt",nrows=1,sep=";"))
-df.power<-read.table(file="\\household_power_consumption.txt", skip=66637,nrows=2880,sep=";",col.names=col.names)
+col.names<-t(read.table(file="household_power_consumption.txt",nrows=1,sep=";"))
+df.power<-read.table(file="household_power_consumption.txt", skip=66637,nrows=2880,sep=";",col.names=col.names)
 
 dateTime <- strptime(paste(df.power$Date,df.power$Time), format="%d/%m/%Y %H:%M:%S")
 Sys.setlocale("LC_TIME", "English")
